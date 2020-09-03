18cc4b3bc5e654ee11b612ccd9bcc09df2bd0d11
Ajay Prajapati <ajayprazz@gmail.com>
2020-09-03 08:28:52 +0545

    get daterange and product names from db

--- report.js
+++ report.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
 const db = require("./models");
+const { QueryTypes } = db.sequelize;
 
 const dataUrl =
   "https://raw.githubusercontent.com/younginnovations/internship-challenges/master/programming/petroleum-report/data.json";
@@ -14,6 +15,8 @@ const dataUrl =
       const data = await db.Petroleum.bulkCreate(jsonData, {
         updateOnDuplicate: ["sale"],
       });
+
+      const report = await createReport();
     } else {
       console.log("Error: ", response.statusText);
     }
@@ -21,3 +24,51 @@ const dataUrl =
     console.log(error);
   }
 })(dataUrl);
+
+const createReport = async () => {
+  let report = [];
+
+  const petroleum_products = await getDistinctProductNames();
+  console.log("product names", petroleum_products);
+
+  const dateRange = await getDateRange();
+  console.log("date range", dateRange);
+
+  return report;
+};
+
+const getDateRange = async () => {
+  const dates = await db.sequelize.query(
+    "select min(year) as minDate, max(year) as maxDate from petroleums",
+    {
+      type: QueryTypes.SELECT,
+    }
+  );
+
+  const minDate = parseInt(dates[0].minDate);
+  const maxDate = parseInt(dates[0].maxDate);
+
+  let dateRange = [];
+
+  for (let date = maxDate; date > minDate; date = date - 5) {
+    dateRange.push({ lower: date - 4, upper: date });
+  }
+
+  return dateRange;
+};
+
+const getDistinctProductNames = async () => {
+  const results = await db.Petroleum.findAll({
+    attributes: [
+      db.Sequelize.fn("DISTINCT", db.Sequelize.col("petroleum_product")),
+      "petroleum_product",
+    ],
+  });
+
+  const petroleum_products = results.map(result => {
+    resultJson = result.toJSON();
+    return resultJson.petroleum_product;
+  });
+
+  return petroleum_products;
+};
